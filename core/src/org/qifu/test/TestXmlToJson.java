e7df5466d75bd719791d8ab0c21c8672a98dcf3d
Bill Chen <chen.xin.nien@gmail.com>
2023-02-24 10:09:01 +0800

    update

--- core/src/org/qifu/test/TestXmlToJson.java
+++ core/src/org/qifu/test/TestXmlToJson.java
@@ -10,9 +10,9 @@ public class TestXmlToJson {
 	
 	public static void main(String args[]) throws Exception {
 		
-		String testXml = "aimi150.xml";
+		//String testXml = "aimi150.xml";
 		//String testXml = "apmt540.xml";
-		//String testXml = "sign_form.xml";
+		String testXml = "sign_form.xml";
 		
 		String formXmlContent = FileUtils.readFileToString(new File("/home/git5/ezflowman/doc/"+testXml), "utf-8");
 		
