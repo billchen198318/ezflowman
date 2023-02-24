7c257166b122d174af15135689ddae5a239e0b5b
Bill Chen <chen.xin.nien@gmail.com>
2023-02-24 15:21:29 +0800

    update

--- core/src/org/qifu/utils/EZFormSupportUtils.java
+++ core/src/org/qifu/utils/EZFormSupportUtils.java
@@ -39,6 +39,17 @@ import com.github.underscore.U;
 public class EZFormSupportUtils implements java.io.Serializable {
 	private static final long serialVersionUID = -7032600196907396427L;
 	
+	private static final String _omitXmlDeclaration = "#omit-xml-declaration";
+	
+	private static final String _element_records = "records";
+	private static final String _element_record = "record";
+	private static final String _element_item = "item";
+	
+	private static final String _attribute_id = "-id";
+	private static final String _attribute_dataType = "-dataType";
+	private static final String _attribute_perDataProId = "-perDataProId";
+	private static final String _attribute_text = "#text";
+	
 	public static EzForm loadFromXml(String formXmlContent) throws Exception {
 		String underscoreJsonStr = U.xmlToJson( formXmlContent );
 		String underscoreXmlStr = U.jsonToXml( underscoreJsonStr ); // test convert to xml		
@@ -46,7 +57,7 @@ public class EZFormSupportUtils implements java.io.Serializable {
 		EzForm ezform = null;
         for (Map.Entry<String, Object> fEntry : formFieldMap.entrySet()) {        	
         	String formId = fEntry.getKey();
-        	if ("#omit-xml-declaration".equals(formId)) {
+        	if (_omitXmlDeclaration.equals(formId)) {
         		continue;
         	}
         	if (StringUtils.isBlank(formId)) {
@@ -65,23 +76,23 @@ public class EZFormSupportUtils implements java.io.Serializable {
         			// -------------------------------------------------------------------------------------
         			// 處理grid
         			boolean isGrid = false;
-        			if (l1Map.get("records") != null && l1Map.get("records") instanceof Map) {
-        				Map<String, Object> l2Map = (Map<String, Object>) l1Map.get("records");
-        				if ( l2Map.get("record") != null && l2Map.get("record") instanceof Map ) {
-        					Map<String, Object> recordMap = (Map<String, Object>) l2Map.get("record");
-        					if (recordMap.get("item") != null && recordMap.get("item") instanceof List) {        						
-        						List< Map<String, Object> > itemList = (List<Map<String, Object>>) recordMap.get("item");
+        			if (l1Map.get(_element_records) != null && l1Map.get(_element_records) instanceof Map) {
+        				Map<String, Object> l2Map = (Map<String, Object>) l1Map.get(_element_records);
+        				if ( l2Map.get(_element_record) != null && l2Map.get(_element_record) instanceof Map ) {
+        					Map<String, Object> recordMap = (Map<String, Object>) l2Map.get(_element_record);
+        					if (recordMap.get(_element_item) != null && recordMap.get(_element_item) instanceof List) {        						
+        						List< Map<String, Object> > itemList = (List<Map<String, Object>>) recordMap.get(_element_item);
         						if (!CollectionUtils.isEmpty(itemList)) {
         							//System.out.println("is grid ===>" + l1Entry.getKey() );
         							EzFormRecord fr = new EzFormRecord();
         							fr.setGridId( l1Entry.getKey() );
-        							fr.setRecordId( StringUtils.defaultString((String)recordMap.get("-id")) );        							
+        							fr.setRecordId( StringUtils.defaultString((String)recordMap.get(_attribute_id)) );        							
         							for (Map<String, Object> itemMap : itemList) {
         								EzFormRecordItem ri = new EzFormRecordItem();
-        								ri.setId( StringUtils.defaultString((String)itemMap.get("-id")) );
-        								ri.setDataType( StringUtils.defaultString((String)itemMap.get("-dataType")) );
-        								ri.setPerDataProId( StringUtils.defaultString((String)itemMap.get("-perDataProId")) );
-        								ri.setText( StringUtils.defaultString((String)itemMap.get("#text")) );
+        								ri.setId( StringUtils.defaultString((String)itemMap.get(_attribute_id)) );
+        								ri.setDataType( StringUtils.defaultString((String)itemMap.get(_attribute_dataType)) );
+        								ri.setPerDataProId( StringUtils.defaultString((String)itemMap.get(_attribute_perDataProId)) );
+        								ri.setText( StringUtils.defaultString((String)itemMap.get(_attribute_text)) );
         								fr.getItems().add(ri);
         							}
         							ezform.getRecords().add(fr);
@@ -95,16 +106,16 @@ public class EZFormSupportUtils implements java.io.Serializable {
         			// -------------------------------------------------------------------------------------
         			// 處理一般 field
         			if (!isGrid) {
-        				if (l1Map.get("-id") != null && l1Map.get("-dataType") != null) {
-        					String fieldId = StringUtils.defaultString((String)l1Map.get("-id"));
+        				if (l1Map.get(_attribute_id) != null && l1Map.get("-dataType") != null) {
+        					String fieldId = StringUtils.defaultString((String)l1Map.get(_attribute_id));
         					if (StringUtils.isBlank(fieldId)) {
         						throw new Exception("form content field no id value.");
         					}        					
         					EzFormField ff = new EzFormField();        					
         					ff.setId( fieldId );
-        					ff.setDataType( StringUtils.defaultString((String)l1Map.get("-dataType")) );
-        					ff.setPerDataProId( StringUtils.defaultString((String)l1Map.get("-perDataProId")) );
-        					ff.setText( StringUtils.defaultString((String)l1Map.get("#text")) );
+        					ff.setDataType( StringUtils.defaultString((String)l1Map.get(_attribute_dataType)) );
+        					ff.setPerDataProId( StringUtils.defaultString((String)l1Map.get(_attribute_perDataProId)) );
+        					ff.setText( StringUtils.defaultString((String)l1Map.get(_attribute_text)) );
         					ezform.getFields().add(ff);        					
         				}        				
         			}
