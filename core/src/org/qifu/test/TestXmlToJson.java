a2e02d2fcb51c943a049da7c59a9e10efed3953e
Bill Chen <chen.xin.nien@gmail.com>
2023-02-24 11:29:16 +0800

    update

--- core/src/org/qifu/test/TestXmlToJson.java
+++ core/src/org/qifu/test/TestXmlToJson.java
@@ -6,6 +6,8 @@ import org.apache.commons.io.FileUtils;
 import org.json.JSONObject;
 import org.json.XML;
 
+import com.github.underscore.U;
+
 public class TestXmlToJson {
 	
 	public static void main(String args[]) throws Exception {
@@ -16,14 +18,24 @@ public class TestXmlToJson {
 		String testXml = "Car_form.xml";
 		
 		String formXmlContent = FileUtils.readFileToString(new File("/home/git5/ezflowman/doc/"+testXml), "utf-8");		
-        JSONObject xmlJSONObj = XML.toJSONObject(formXmlContent);        
         
+		/*
+		JSONObject xmlJSONObj = XML.toJSONObject(formXmlContent);                
         System.out.println("--------------------------------------------------------------------");
         System.out.println( xmlJSONObj );		
         System.out.println("--------------------------------------------------------------------");
 		
         String json2xml = XML.toString( xmlJSONObj );
         System.out.println( json2xml );
+        */
+		
+		String jsonStr = U.xmlToJson( formXmlContent );
+        System.out.println("--------------------------------------------------------------------");
+        System.out.println( jsonStr );		
+        System.out.println("--------------------------------------------------------------------");        
+        
+        String xmlStr = U.jsonToXml( jsonStr );
+        System.out.println( xmlStr );		
         
 	}
 	
