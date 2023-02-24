27e529e82afc487b59bcd7866997600bab4ee716
Bill Chen <chen.xin.nien@gmail.com>
2023-02-24 10:23:48 +0800

    update

--- core/src/org/qifu/test/TestXmlToJson.java
+++ core/src/org/qifu/test/TestXmlToJson.java
@@ -5,6 +5,7 @@ import java.io.File;
 import org.apache.commons.io.FileUtils;
 import org.json.JSONObject;
 import org.json.XML;
+import org.json.XMLParserConfiguration;
 
 public class TestXmlToJson {
 	
@@ -14,12 +15,20 @@ public class TestXmlToJson {
 		//String testXml = "apmt540.xml";
 		String testXml = "sign_form.xml";
 		
-		String formXmlContent = FileUtils.readFileToString(new File("/home/git5/ezflowman/doc/"+testXml), "utf-8");
-		
-        JSONObject xmlJSONObj = XML.toJSONObject(formXmlContent);
+		String formXmlContent = FileUtils.readFileToString(new File("/home/git5/ezflowman/doc/"+testXml), "utf-8");		
+        JSONObject xmlJSONObj = XML.toJSONObject(formXmlContent);        
+        
+        System.out.println("--------------------------------------------------------------------");
         System.out.println( xmlJSONObj );		
+        System.out.println("--------------------------------------------------------------------");
 		
-		
+        XMLParserConfiguration xpc = new XMLParserConfiguration();
+        //xpc.withcDataTagName("id");
+        //xpc.withcDataTagName("dataType");
+        xpc.withKeepStrings(true);        
+        String json2xml = XML.toString( xmlJSONObj , null, xpc );
+        System.out.println( json2xml );
+        
 	}
 	
 }
