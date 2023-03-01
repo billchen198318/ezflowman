35206edef9be0f48c20eb3fffaec79ece2c9e352
billchen198318 <chen.xin.nien@gmail.com>
2023-03-01 19:37:42 +0800

    update

---
+++ core/resources/templates/view/ezf_ds/create-page.ftl
@@ -0,0 +1,54 @@
+<html>
+<head>
+<title>EFGP整合配置</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<#import "../common-f-inc.ftl" as cfi />
+<@cfi.commonFormHeadResource /> 
+
+<script type="text/javascript" src="${qifu_basePath}js/vue.global.js"></script>
+
+
+<style type="text/css">
+
+
+
+</style>
+
+
+<script type="text/javascript">
+
+$( document ).ready(function() {
+	
+	
+});
+
+</script>
+
+</head>
+
+<body>
+
+<#import "../common-f-head.ftl" as cfh />
+<@cfh.commonFormHeadContent /> 
+
+<div class="form-group" id="form-group1">	
+	<div class="row">	
+		<div id="main-content" class="col-xs-12 col-md-12 col-lg-12">
+		
+		
+		
+		</div>
+	</div>
+</div>		
+
+
+<br/>
+<br/>
+<br/>
+
+<script type="text/javascript" src="${qifu_basePath}js/ezf/EZF_A001D0009A.js?ver=${qifu_jsVerBuild}"></script>
+
+</body>
+</html>		
---
+++ core/src/org/qifu/core/controller/EzDsConfigController.java
@@ -0,0 +1,55 @@
+package org.qifu.core.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.qifu.base.controller.BaseControllerSupport;
+import org.qifu.base.controller.IPageNamespaceProvide;
+import org.qifu.base.exception.AuthorityException;
+import org.qifu.base.exception.ControllerException;
+import org.qifu.base.exception.ServiceException;
+import org.qifu.base.model.ControllerMethodAuthority;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class EzDsConfigController extends BaseControllerSupport implements IPageNamespaceProvide {
+
+	@Override
+	public String viewPageNamespace() {
+		return "ezf_ds";
+	}
+	
+	private void init(String type, ModelMap mm) throws AuthorityException, ControllerException, ServiceException, Exception {
+		if ("mainPage".equals(type)) {
+			
+		}
+		if ("createPage".equals(type)) {
+			
+		}
+	}
+	
+	@SuppressWarnings("unused")
+	private void fetch(ModelMap mm, String oid) throws AuthorityException, ControllerException, ServiceException, Exception {
+		
+	}
+	
+	@ControllerMethodAuthority(check = true, programId = "EZF_A001D0009A")
+	@RequestMapping("/ezfDsConfigPage")
+	public String mainPage(ModelMap mm, HttpServletRequest request) {
+		String viewName = this.viewCreatePage();
+		this.getDefaultModelMap(mm, this.currentMethodAuthority());
+		try {
+			this.init("createPage", mm);
+		} catch (AuthorityException e) {
+			viewName = this.getAuthorityExceptionPage(e, mm);
+		} catch (ControllerException | ServiceException e) {
+			viewName = this.getServiceOrControllerExceptionPage(e, mm);
+		} catch (Exception e) {
+			viewName = this.getExceptionPage(e, mm);
+		}
+		return viewName;
+	}	
+	
+	
+}
