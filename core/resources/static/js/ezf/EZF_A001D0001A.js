355d6e4ae53401972cb06687bcee85bbc5b229e9
Bill Chen <chen.xin.nien@gmail.com>
2023-03-01 13:16:15 +0800

    update

---
+++ core/resources/static/js/ezf/EZF_A001D0001A.js
@@ -0,0 +1,28 @@
+var _ezform = [];
+
+const PageEventHandling = {
+	data() {
+		return {
+			ezform	:	_ezform
+		}
+	},
+	methods: {
+		initData	:	_initData,
+		clearPage	:	_clearPage
+	},
+	mounted() {
+		this.initData();
+	},
+	computed: {
+		
+	}
+}
+
+function _initData() {
+	
+}
+
+function _clearPage() {
+	
+}
+
