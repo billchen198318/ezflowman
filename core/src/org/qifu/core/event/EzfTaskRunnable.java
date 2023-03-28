d025ce732b5360bb7c31d87f5d1d1c317990c1a9
Bill Chen <chen.xin.nien@gmail.com>
2023-03-28 17:13:12 +0800

    update

--- core/src/org/qifu/core/event/EzfTaskRunnable.java
+++ core/src/org/qifu/core/event/EzfTaskRunnable.java
@@ -1,8 +1,35 @@
 package org.qifu.core.event;
 
+import org.qifu.base.AppContext;
+import org.qifu.base.exception.ServiceException;
+import org.qifu.core.entity.EzfDs;
+import org.qifu.core.entity.EzfMap;
+import org.qifu.core.entity.EzfMapField;
+import org.qifu.core.entity.EzfMapGrd;
+import org.qifu.core.entity.EzfMapGrdTblMp;
+import org.qifu.core.logic.IEzfMapperLogicService;
+import org.qifu.core.service.IEzfDsService;
+import org.qifu.core.service.IEzfMapFieldService;
+import org.qifu.core.service.IEzfMapGrdService;
+import org.qifu.core.service.IEzfMapGrdTblMpService;
+import org.qifu.core.service.IEzfMapService;
+import org.springframework.beans.BeansException;
+
 public class EzfTaskRunnable implements Runnable {
 	
 	private String cnfId;
+	
+	private IEzfDsService<EzfDs, String> ezfDsService;
+	
+	private IEzfMapService<EzfMap, String> ezfMapService;
+	
+	private IEzfMapFieldService<EzfMapField, String> ezfMapFieldService;	
+	
+	private IEzfMapGrdService<EzfMapGrd, String> ezfMapGrdService;
+	
+	private IEzfMapGrdTblMpService<EzfMapGrdTblMp, String> ezfMapGrdTblMpService;	
+	
+	private IEzfMapperLogicService ezfMapperLogicService;
 	
 	public EzfTaskRunnable() {
 		super();
@@ -13,8 +40,42 @@ public class EzfTaskRunnable implements Runnable {
 		this.cnfId = cnfId;
 	}
 	
+	private void initBeans() throws BeansException, Exception {
+		if (null == this.ezfDsService) {
+			this.ezfDsService = (IEzfDsService<EzfDs, String>) AppContext.getBean(IEzfMapService.class);
+		}
+		if (null == this.ezfMapService) {
+			this.ezfMapService = (IEzfMapService<EzfMap, String>) AppContext.getBean(IEzfMapService.class);
+		}
+		if (null == this.ezfMapFieldService) {
+			this.ezfMapFieldService = (IEzfMapFieldService<EzfMapField, String>) AppContext.getBean(IEzfMapFieldService.class);
+		}
+		if (null == this.ezfMapGrdService) {
+			this.ezfMapGrdService = (IEzfMapGrdService<EzfMapGrd, String>) AppContext.getBean(IEzfMapGrdService.class);
+		}
+		if (null == this.ezfMapGrdTblMpService) {
+			this.ezfMapGrdTblMpService = (IEzfMapGrdTblMpService<EzfMapGrdTblMp, String>) AppContext.getBean(IEzfMapGrdTblMpService.class);
+		}
+		if (null == this.ezfMapperLogicService) {
+			this.ezfMapperLogicService = (IEzfMapperLogicService) AppContext.getBean(IEzfMapperLogicService.class);
+		}
+	}
+	
 	@Override
 	public void run() {
+		try {
+			this.initBeans();
+			this.process();			
+		} catch (BeansException e) {
+			e.printStackTrace();
+		} catch (ServiceException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void process() throws ServiceException, Exception {
 		System.out.println("cnfId>>>" + this.cnfId);
 	}
 	
